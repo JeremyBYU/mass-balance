ba8f08afb359712b31b3e6759b57fa5cb6f26167
Jeremy Castagno <JeremyBYU@gmail.com>
2016-08-13 08:24:22 -0400

    Initial Commit

---
+++ src/vendor.ts
@@ -0,0 +1,5 @@
+// Get webpack to automatically include bootstrap
+// If you have more vendor stuff, you should probably get webpack to make a second bundle
+
+// require('bootstrap/dist/css/bootstrap.min.css')
+// require('bootstrap/dist/js/bootstrap.min.js')
